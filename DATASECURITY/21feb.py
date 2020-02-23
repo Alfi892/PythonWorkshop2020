3ced07d1c39ad46f34a75875d50ed0f101676d34
Alfi892 <61371311+Alfi892@users.noreply.github.com>
2020-02-23 11:35:32 +0530

    Add files via upload

---
+++ DATASECURITY/21feb.py
@@ -0,0 +1,12 @@
+import socket
+remoteServer = input("enter the port to scan:")
+
+for port in range(1,1001):
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    result = sock.connect_ex((remoteServer,port))
+    if result ==0:
+        print("Port{}:Open".format(port))
+    else:
+        print("Port{}:Close".format(port))
+    sock.close()
+
